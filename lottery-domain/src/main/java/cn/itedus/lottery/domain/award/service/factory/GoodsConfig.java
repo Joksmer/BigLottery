26adc9536e998338cb315a3121808ae8e77fa876
小傅哥 <184172133@qq.com>
2022-02-11 18:33:37 +0800

    恢复，原始发奖注入配置，为体现不同方式的注入，不要修改此代码。分支已设置保护。

--- lottery-domain/src/main/java/cn/itedus/lottery/domain/award/service/factory/GoodsConfig.java
+++ lottery-domain/src/main/java/cn/itedus/lottery/domain/award/service/factory/GoodsConfig.java
@@ -16,19 +16,28 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 public class GoodsConfig {
 
-    /**
-     * 奖品发放策略组
-     */
+    /** 奖品发放策略组 */
     protected static Map<Integer, IDistributionGoods> goodsMap = new ConcurrentHashMap<>();
-    @Resource
-    private Map<String,IDistributionGoods> beanNameMap;
 
+    @Resource
+    private DescGoods descGoods;
+
+    @Resource
+    private RedeemCodeGoods redeemCodeGoods;
+
+    @Resource
+    private CouponGoods couponGoods;
+
+    @Resource
+    private PhysicalGoods physicalGoods;
 
     @PostConstruct
     public void init() {
-        beanNameMap.entrySet().forEach(entry -> {
-            goodsMap.put(entry.getValue().getDistributionGoodsName(), entry.getValue());
-        });
+        goodsMap.put(Constants.AwardType.DESC.getCode(), descGoods);
+        goodsMap.put(Constants.AwardType.RedeemCodeGoods.getCode(), redeemCodeGoods);
+        goodsMap.put(Constants.AwardType.CouponGoods.getCode(), couponGoods);
+        goodsMap.put(Constants.AwardType.PhysicalGoods.getCode(), physicalGoods);
     }
 
 }
+
